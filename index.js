555fc201ac589d83a1b1db9ca20a76638d6ccf68
Lauren Hoock <laurenh8916@protonmail.com>
2022-04-03 15:47:17 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(names, searchTerm){
+    function testMatch(element){
+        return element.toLowerCase() === searchTerm.toLowerCase();
+    }
+
+    return names.filter(testMatch);
+}
+
+function fuzzyMatch(names, searchTerm){
+    function testMatch(element){
+        return searchTerm === element.slice(0, searchTerm.length);
+    }
+
+    return names.filter(testMatch);
+}
+
+function matchName(drivers, searchTerm){
+    function testMatch(element){
+        return element.name === searchTerm;
+    }
+
+    return drivers.filter(testMatch);
+}
